873bfe4b949edf0b54922d86f106c184d81b58ee
kiran <kiran04.b@gmail.com>
2025-06-19 02:24:10 +0000

    updated-code

--- Myapp.java
+++ Myapp.java
@@ -2,6 +2,9 @@ public class Myapp {
 	pulic static void main(String[] args){
 		int a=10;
 		int b=20;
-		System.out.println("Addition is:",+(a+b));
+		int c=30;
+		int d=40;
+		System.out.println("Addition is:",+(a+b+c+d));
+		System.out.println("The task completed by kiran");
 	}
 }
