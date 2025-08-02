2ad2d66f221ab338eb79ae7e8b143626aa71eaa9
tejayarra5566 <tejayarra99@gmail.com>
2025-08-02 20:45:18 +0530

    Update Myapp.java

--- Myapp.java
+++ Myapp.java
@@ -1,10 +1,11 @@
 public class Myapp {
 	public static void main(String[] args){
-		int a=10;
-		int b=20;
-		int c=30;
-		int d=40;
-		System.out.println("Addition is:",+(a+b+c+d));
-		System.out.println("The task completed by kiran");
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		int d = 40;
+		System.out.println("Addition is: " + (a + b + c + d));
+		System.out.println("The task completed by Kiran");
 	}
 }
+
